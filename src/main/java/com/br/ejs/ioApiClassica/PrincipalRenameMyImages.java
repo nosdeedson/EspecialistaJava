e2ffa3ce9a5a25befe60edd28035fd44a27b02a9
nosdeedson <josemariazumira@gmail.com>
2023-05-01 22:32:04 -0300

    a little change

--- src/main/java/com/br/ejs/ioApiClassica/PrincipalRenameMyImages.java
+++ src/main/java/com/br/ejs/ioApiClassica/PrincipalRenameMyImages.java
@@ -1,36 +1,52 @@
 package com.br.ejs.ioApiClassica;
 
 import java.io.File;
+import java.util.*;
 
 public class PrincipalRenameMyImages {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        renamingFilesRandomNumbersName();
         renamingFilesFromFolderVideos();
     }
 
-    private static void renamingFilesFromFolderVideos() {
+    private static void renamingFilesRandomNumbersName(){
         File videos = new File("/home/edson/Vídeos");
         File[] files = videos.listFiles(File::isFile);
-//        Set<Integer> mix = new HashSet<>(1000);
-//        Random random = new Random();
-
-        long name = 9548;
+        Set<Integer> mix = new HashSet<>(1000);
+        Random random = new Random();
         if (files != null){
-//            for (int i = 0; i < files.length + 100; i++) {
-//                mix.add(Integer.valueOf(random.nextInt(1000) * random.nextInt(1000)));
-//            }
-//            List<Integer> mixList = new ArrayList<>(mix);
-//            System.out.println(mixList);
+            for (int i = 0; i < files.length + 100; i++) {
+                mix.add(Integer.valueOf(random.nextInt(1000) * random.nextInt(1000)));
+            }
+            List<Integer> mixList = new ArrayList<>(mix);
             for (int i = 0; i < files.length; i++) {
-                File newName = new File("/home/edson/Vídeos/" +  name);
+                File newName = new File("/home/edson/Vídeos/" + mixList.get(i));
                 try {
-//                    System.out.println(files[i].getName());
                     System.out.println(files[i].renameTo(newName));
-                    name++;
                 } catch (RuntimeException e){
                     e.printStackTrace();
                 }
             }
-//            System.out.println(mix);
+        }
+    }
+
+    private static void renamingFilesFromFolderVideos() throws InterruptedException {
+        System.out.println("##################### RENAMING TO PUT IN IMAGES ###################");
+        File videos = new File("/home/edson/Vídeos");
+        File[] files = videos.listFiles(File::isFile);
+        // always change
+        long name = 9801;
+        files = videos.listFiles(File::isFile);
+        if (files != null){
+            for (int i = 0; i < files.length; i++){
+                File newName = new File("/home/edson/Vídeos/" +  name);
+                try {
+                    System.out.println(String.valueOf(files[i].renameTo(newName)) + " = " + name);
+                    name++;
+                }catch (RuntimeException e){
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
